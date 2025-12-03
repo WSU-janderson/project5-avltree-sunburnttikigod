c76ad1956777aa3eae6ce71db56d67b5582fc7ef
Charlie Must <must.2@wright.edu>
2025-12-03 10:07:21 -0500

    init commit and push my code, before mysterious shitty code was  maliciously injected curiously between the commit branched and the time i got home, luckily my laptop was not updated since the proffessors outrageous display of aggression , when he suspiciously showed the exact code that suddenly manifested in my code when i returned home from class on the 19th very interesting indeed

--- AVLTree.h
+++ AVLTree.h
@@ -1,31 +1,10 @@
-/***
-   * MY MY MY MY MY MY MY MY MY MY
-   * MY AVLTree.h [header file] MY
-   * MY MY MY MY MY MY MY MY MY MY
-   * Created by
-   * ©️ĥª®Ł¡ē↴ ™️
-   * MMMMMM       MMMMMM UUUUUU   UUUUUU     SSSSSSSS      TTTTTTTT /T|\T /    ©️
-   *  MMMMMMM   MMMMMMM   UUUU     UUUU    SSSSS  SSSSS    TTTTTTTTTT/ T T_ T
-   *  MMMM MMM MMM MMMM   UUUU     UUUU   SSSSS     SSSS   TT    TTT  -X T\|
-   *  MMMM  MMMMM  MMMM   UUUU     UUUU    SSSS        S   T     TTT    L \ X
-   *  MMMM   MMM   MMMM   UUUU     UUUU   S    SSSSS    S        TTT   T T  L
-   *  MMMM         MMMM   UUUU     UUUU   SS       SSSS  S       TTT  T  /|  T|-
-   *  MMMM         MMMM   UUUU     UUUU   SSS      SSSSS         TTT    T - \  | -
-   *  MMMM         MMMM    UUUUUUUUUUU  SS SSSS   SSSSS         TTTTT     \Z||/-=__I
-   * MMMMMM       MMMMMM    UUUUUUUUU   S  S SSSSSSSS          TTTTTTTT  TX\_|_T[|_T
-   *
-   * 01000011 01101000 01100001 01110010 01101100 01100101 01110011
-   * 01001010 01101111 01110011 01100101 01110000 01101000
-   * 01001101 01110101 01110011 01110100
-   * AVLTree.h
-****/
+/**
+ * AVLTree.h
+ */
 
 #ifndef AVLTREE_H
 #define AVLTREE_H
 #include <string>
-#include <vector>
-#include <cmath>
-#include <optional>
 
 using namespace std;
 
@@ -40,98 +19,40 @@ protected:
         KeyType key;
         ValueType value;
         size_t height;
+        size_t childCount;
+        bool familyIsTight = true;
+        size_t currentNumChildren = 0;
 
-        AVLNode *left;
-        AVLNode *right;
+        AVLNode* left;
+        AVLNode* right;
 
-        std::string getValue() const;
-
+        size_t findChild(const AVLNode* sideChild) const;
         // 0, 1 or 2
         size_t numChildren() const;
-
         // true or false
         bool isLeaf() const;
-
         // number of hops to deepest leaf node
         size_t getHeight() const;
+
+
     };
 
 public:
 
-private:
-    AVLNode *root;
-    vector<KeyType> keyList;
-    size_t maxBalancedHeight;
 
-    /*
-     *  insert + its recursive helper declarations
-    */
-    bool insert(const std::string &key, size_t value);
 
-    bool recursivelyInsert(AVLNode *&nodeIn, const std::string &key, size_t value);
 
-    vector<std::string> findRange(const std::string &lowKey, const std::string &highKey);
-
-    /*
-     *  findRange + its recursive helper declarations
-    */
-    std::vector<std::string> findRange(const std::string &lowKey, const std::string &highKey) const;
-
-    void rangingRecursively(AVLNode *ranger, const std::string &lowKey,
-                            const std::string &highKey, vector<std::string> *&rangeList);
-
-    /*
-     *  get + its recursive helper declarations
-    */
-    std::optional<size_t> get(const std::string &key) const;
-
-    std::optional<size_t> getRecursively(AVLNode *currentNode, const std::string &key) const;
-
-    /*
-     *  balancing function declarations
-    */
-    void balanceNode(AVLNode *&node);
-
-    int getBalance(AVLNode *&node) const;
-
-    void rotateLeft(AVLNode *&node);
-
-    void rotateRight(AVLNode *&node);
+    private:
+    AVLNode* root;
 
     /* Helper methods for remove */
     // this overloaded remove will do the recursion to remove the node
-    bool remove(AVLNode *&current, KeyType key);
-
+    bool remove(AVLNode*& current, KeyType key);
     // removeNode contains the logic for actually removing a node based on the numebr of children
-    bool removeNode(AVLNode *&current);
+    bool removeNode(AVLNode*& current);
+    // You will implement this, but it is needed for removeNode()
+    void balanceNode(AVLNode*& node);
 
-    // The bracket operator overload
-
-    size_t operator[](const std::string &key);
-
-    /*
-     * contains and it's recursion function
-    */
-
-    bool contains(const std::string &key) const;
-
-    bool containerRecurse(const AVLNode &container, const std::string &key) const;
-
-    vector<KeyType> keys() const;
-
-    size_t size() const;
-
-    size_t getMaxBalancedHeight();
-
-    size_t getHeight() const;
-
-    std::string getValue() const;
-
-    void operator=(const AVLTree &other);
-
-    ~AVLTree();
-
-    friend std::ostream &operator<<(ostream &os, const AVLTree &avlTree);
 };
 
 #endif //AVLTREE_H
